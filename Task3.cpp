3288f8c94198d826b8139c9b2a2b43eb7967f153
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-10-18 19:44:14 +0300

    Task created about *

---
+++ Task3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+	int n;
+	std::cout << "Input n: ";
+	std::cin >> n;
+	for (int i = n; i > 0; --i) {
+		for (int p = i - 1; p > 0; --p) {
+			std::cout << " ";
+		}
+		for (int j = n - i + 1; j > 0; --j) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
