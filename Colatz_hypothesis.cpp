5f184474cc94dfcd19a24f382c725e2583af4905
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-10-18 19:52:30 +0300

    Implementetion of hypothesis calculation up to 1

---
+++ Colatz_hypothesis.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        cout << "n: " << n << endl;
+    }
+}
