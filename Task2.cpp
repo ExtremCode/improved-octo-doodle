8ab35a3d31481a6be059be877917b549e9f153d3
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-10-18 19:38:26 +0300

    Add reverse piramid from task1

---
+++ Task2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Input n ";
+    cin >> n;
+    for (int i = n; i > 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
