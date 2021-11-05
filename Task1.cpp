c2fa687e3a7adf68f3c56427a0e8a90f45ca805b
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-11-05 22:49:14 +0300

    Solution added of array of the hexadecimal numbers

--- Task1.cpp
+++ Task1.cpp
@@ -1,15 +1,33 @@
 ﻿#include <iostream>
-using namespace std;
+#include <time.h>
+#include <string>
+#include <vector>
 
 int main()
 {
+    srand(time(0));
+    char m[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
     int n;
-    cout << "input n " << " ";
-    cin >> n;
+    std::cout << "Enter the number of elemnts of the array ";
+    std::cin >> n;
+    std::cout << std::endl;
+    std::vector <std::string> arr(n);
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            cout << "*";
-        }cout << endl;
+        int tmp = rand() % 1000;
+        std::string d;
+        while (tmp > 0) {
+            d += m[tmp % 16];
+            tmp /= 16;
+        }
+        for (int i2 = d.length() - 1; i2 >= 0; --i2) {
+            arr[i] += d[i2];
+        }
     }
-    return 0;
+    std::cout << "Your array of hexadecimal numbers: " << std::endl;
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    system("pause");
 }
