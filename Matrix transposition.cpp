6776b7f4307e8796c62e8b1e7a3917ed8862a0fb
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-11-06 14:25:14 +0300

    Solution added of transposition any matrix

---
+++ Matrix transposition.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <time.h>
+
+std::vector< std::vector <int> > trm(std::vector< std::vector <int> > p, size_t nr, size_t nc) {
+	std::vector<std::vector <int>> nw(nc,std::vector<int> (nr,0));
+	for (size_t r = 0; r < nr; ++r) {
+		for (size_t c = 0; c < nc; ++c) {
+			nw[c][r] = p[r][c];
+		}
+	}
+	return nw;
+}
+
+int main()
+{
+	srand(time(NULL));
+	std::cout << "Enter numbers of rows and columns ";
+	int r, c;
+	std::cin >> r >> c;
+	std::vector <std::vector <int> > arr(r, std::vector<int> (c,0));
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			arr[i][j] = rand() % 100;
+		}
+	}
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			std::cout<<arr[i][j]<<' ';
+		}
+		std::cout << std::endl;
+	}
+	arr=trm(arr, r, c);
+	std::cout << "Transoposed matrix " << std::endl;
+	for (int i = 0; i < c; ++i) {
+		for (int j = 0; j < r; ++j) {
+			std::cout << arr[i][j] << ' ';
+		}
+		std::cout << std::endl;
+	}
+}
