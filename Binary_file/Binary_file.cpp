6ad31e316c9d9744ccdf3f362f754d1f7527db11
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-12-18 12:31:04 +0300

    Writing to binary file and reading it array

--- Binary_file/Binary_file.cpp
+++ Binary_file/Binary_file.cpp
@@ -1,6 +1,71 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+
+bool test(const std::string argv){
+    std::fstream file;
+    file.open(argv, std::ios_base::out  | std::ios_base::trunc | std::ios_base::binary | std::ios_base::in);
+    if (file.is_open()) {
+        std::cout << "Open" << std::endl;
+        std::vector<int> test = { 9,8,7,6,5 };
+        std::vector<int> m = { 1,2,3,4,5 };
+        for (size_t j = 0; j < m.size(); ++j) {
+            file.write((char*)&m[j], sizeof(int));
+        }
+        file.close();
+        file.open(argv,std::ios_base::binary | std::ios_base::in);
+        if (file.is_open()) {
+            for (size_t i = 0; i < m.size(); ++i) {
+                file.read((char*)&test[i], sizeof(int));
+            }
+        }file.close();
+        if(m!=test) return false;
+        
+        m = { -1,-2,-3,-4,-10,67,35};
+        test.resize(m.size(),0);
+        for (size_t j = 0; j < m.size(); ++j) {
+            file.write((char*)&m[j], sizeof(int));
+        }
+        file.close();
+        file.open(argv,std::ios_base::binary | std::ios_base::in);
+        if (file.is_open()) {
+            for (size_t i = 0; i < m.size(); ++i) {
+                file.read((char*)&test[i], sizeof(int));
+            }
+        }file.close();
+        if(m!=test) return false;
+        
+        std::vector<std::vector<int>> nw = {{ -1,-2,-3,-4,-10,67,35},{ -1,-2,-3,-4,-10,67,35},{ -1,-2,-3,-4,-10,67,35}};
+        std::vector<std::vector<int>> nwtest(nw.size(),std::vector<int>(nw[0].size(),0));
+        for (size_t j = 0; j < nw.size(); ++j) {
+            file.write((char*)&nw[j], 7*sizeof(int));
+        }
+        file.close();
+        file.open(argv,std::ios_base::binary | std::ios_base::in);
+        if (file.is_open()) {
+            for (size_t i = 0; i < nw.size(); ++i) {
+                file.read((char*)&nwtest[i], 7*sizeof(int));
+            }
+        }file.close();
+        if(nw!=nwtest) return false;
+        
+        nw = {{ -1,100,-3,0,-10,67,6,-12},{ -1,-2,-3,-4,-10,32,35,96},{ -1,-2,-3,-9,-10,67,35,0}};
+        nwtest.resize(nw.size(),std::vector<int>(nw[0].size(),0));
+        for (size_t j = 0; j < nw.size(); ++j) {
+            file.write((char*)&nw[j], 7*sizeof(int));
+        }
+        file.close();
+        file.open(argv,std::ios_base::binary | std::ios_base::in);
+        if (file.is_open()) {
+            for (size_t i = 0; i < nw.size(); ++i) {
+                file.read((char*)&nwtest[i], 7*sizeof(int));
+            }
+        }file.close();
+        if(nw!=nwtest) return false;
+    }
+    return true;
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,4 +89,5 @@ int main(int argc, char* argv[])
     }
     else { std::cout << "Error"; }
     file.close();
+    std::cout<<test("Example.txt")<<std::endl;
 }
