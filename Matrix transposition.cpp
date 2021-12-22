7198d1f0af5b437c87528e00dd599097417fff30
ExtremCode <91559211+ExtremCode@users.noreply.github.com>
2021-12-22 03:35:35 +0300

    Update Matrix transposition.cpp

--- Matrix transposition.cpp
+++ Matrix transposition.cpp
@@ -21,6 +21,102 @@ int* transp_mat_depend(int *p, size_t nr, size_t nc) {
 	return nw;
 }
 
+bool test_transp_mat_indep() {
+	size_t nr = 2, nc = 2;
+	int** test = new int* [nr]; int** ori = new int*[nc];
+	for (size_t r = 0; r < nr; ++r) {
+		test[r] = new int[nc];
+	}
+	for (size_t c = 0; c < nc; ++c) {
+		ori[c] = new int[nr];
+	}
+	test[0][0] = 1, test[0][1] = 2, test[1][0] = -3, test[1][1] = 4;
+	ori[0][0] = 1, ori[0][1] = -3, ori[1][0] = 2, ori[1][1] = 4;
+	test=transp_mat_indep(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c][r] != test[c][r]) return false;
+		}
+	}
+
+	nr = 2, nc = 3;
+	test = new int*[nr], ori = new int*[nc];
+	for (size_t r = 0; r < nr; ++r) {
+		test[r] = new int[nc];
+	}
+	for (size_t c = 0; c < nc; ++c) {
+		ori[c] = new int[nr];
+	}
+	test[0][0] = 1, test[0][1] = 2, test[0][2] = -3, test[1][0] = 0, test[1][1] = 9, test[1][2] = 43;
+	ori[0][0] = 1, ori[0][1] = 0, ori[1][0] = 2, ori[1][1] = 9, ori[2][0] = -3, ori[2][1] = 43;
+	test=transp_mat_indep(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c][r] != test[c][r]) return false;
+		}
+	}
+
+	nr = 3, nc = 3;
+	test = new int* [nr], ori = new int* [nc];
+	for (size_t r = 0; r < nr; ++r) {
+		test[r] = new int[nc];
+	}
+	for (size_t c = 0; c < nc; ++c) {
+		ori[c] = new int[nr];
+	}
+	test[0][0] = 10, test[0][1] = 14, test[0][2] = 35, test[1][0] = -96, test[1][1] = 65, test[1][2] = 34, 
+	test[2][0] = 0, test[2][1] = 55, test[2][2] = 43;
+	ori[0][0] = 10, ori[0][1] = -96, ori[0][2] = 0, ori[1][0] = 14, ori[1][1] = 65, ori[1][2] = 55,
+	ori[2][0] = 35, ori[2][1] = 34, ori[2][2] = 43;
+	test=transp_mat_indep(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c][r] != test[c][r]) return false;
+		}
+	}
+
+	return true;
+}
+
+bool test_transp_mat_depen() {
+	size_t nr = 2, nc = 2;
+	int* test = new int[nr*nc]; int* ori = new int[nc*nc];
+	test[0 * nc + 0] = 1, test[0 * nc + 1] = 2, test[1 * nc + 0 ] = -3, test[1 * nc + 1] = 4;
+	ori[0 * nr + 0] = 1, ori[0 * nr + 1] = -3, ori[1 * nr+0] = 2, ori[1 * nr + 1] = 4;
+	test = transp_mat_depend(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c * nr + r] != test[c * nr + r]) return false;
+		}
+	}
+
+	nr = 2, nc = 3;
+	test = new int[nr*nc], ori = new int[nc*nr];
+	test[0 * nc + 0] = 1, test[0 * nc + 1] = 2, test[0 * nc + 2] = -3, test[1 * nc + 0] = 0, test[1 * nc + 1] = 9, test[1 * nc + 2] = 43;
+	ori[0 * nr + 0] = 1, ori[0 * nr + 1] = 0, ori[1 * nr + 0] = 2, ori[1 * nr + 1] = 9, ori[2 * nr + 0] = -3, ori[2 * nr + 1] = 43;
+	test = transp_mat_depend(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c * nr + r] != test[c * nr + r]) return false;
+		}
+	}
+
+	nr = 3, nc = 3;
+	test = new int[nr*nc], ori = new int[nc*nr];
+	test[0*nc+0] = 10, test[0 * nc + 1] = 14, test[0 * nc + 2] = 35, test[1 * nc + 0] = -96, test[1 * nc + 1] = 65, test[1 * nc + 2] = 34,
+	test[2 * nc + 0] = 0, test[2 * nc + 1] = 55, test[2 * nc + 2] = 43;
+	ori[0 * nr + 0] = 10, ori[0 * nr + 1] = -96, ori[0 * nr + 2] = 0, ori[1 * nr + 0] = 14, ori[1 * nr + 1] = 65, ori[1 * nr + 2] = 55,
+	ori[2 * nr + 0] = 35, ori[2 * nr + 1] = 34, ori[2 * nr + 2] = 43;
+	test = transp_mat_depend(test, nr, nc);
+	for (size_t c = 0; c < nc; ++c) {
+		for (size_t r = 0; r < nr; ++r) {
+			if (ori[c*nr+r] != test[c*nr+r]) return false;
+		}
+	}
+
+	return true;
+}
+
 int main()
 {
 	srand(time(NULL));
