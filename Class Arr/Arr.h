70fafe08ea84514baf40977d274da7eb95c53ccb
Leff <klj2018@yandex.ru>
2022-03-22 00:11:18 +0300

    something adding

--- Class Arr/Arr.h
+++ Class Arr/Arr.h
@@ -5,25 +5,45 @@ struct BadSize {};
 struct BadGetElement {};
 struct BadRemove {};
 
-template<typename T>
-class Arr {
+template <typename T>
+class Marker {
 	T* m_storage;
 	size_t m_size;
+protected:
+	T* m_cur_marker;
+public:
+	Marker(T* current = nullptr, T* store = nullptr, size_t size = 0) :
+		m_cur_marker(current), m_storage(store), m_size(size) {}
+	T& getcur_val();
+	void rewind();
+	bool canMove2next();
+	void move2next();
+	void fast_forward();
+};
+
+template<typename T>
+class Arr : public Marker<T> {
+	T* m_storage;
+	size_t m_size;
+	Marker<T> mark{ m_storage, m_storage, m_size };
 public:
 	Arr();
 	Arr(int);
 	~Arr();
 	T removeElement(const size_t);
 	T& getElement(const size_t) const;
-	size_t getSize() const;
+	inline size_t getSize() const;
 	void addElement(const T);
 	void Resize_arr(const size_t);
+	T* getMarker() {
+		return mark.m_cur_marker;
+	}
 };
 
 template <typename T>
 Arr<T>::Arr() {
-	m_size = 0;
-	m_storage = nullptr;
+	/*mark.m_size = */m_size = 0;
+	/*mark.m_cur_marker = mark.m_storage = */m_storage = nullptr;
 }
 
 template <typename T>
@@ -32,14 +52,14 @@ Arr<T>::Arr(int size) {
 		throw BadSize();
 	}
 	else {
-		m_size = size;
-		m_storage = new T[m_size];
+		/*mark.m_size = */m_size = size;
+		/*mark.m_cur_marker = mark.m_storage = */m_storage = new T[m_size];
 	}
 }
 
 template <typename T>
 Arr<T>::~Arr() {
-	if (m_storage != nullptr) {
+	if (m_storage) {
 		delete[] m_storage;
 	}
 }
@@ -49,22 +69,23 @@ T Arr<T>::removeElement(const size_t index) {
 	T parametr;
 	if (m_size > index) {
 		parametr = m_storage[index];
-		T* tmp = new T[m_size-1];
-		for (size_t j = 0, k=0; k < m_size; ++j) {
+		T* tmp = new T[m_size - 1];
+		for (size_t j = 0, k = 0; k < m_size; ++j) {
 			if (j != index) {
-				tmp[j] = m_storage[k++];
-			} else {
-				tmp[j] = m_storage[++k];
-				++k;
+				tmp[k++] = m_storage[j];
+			}
+			else {
+				if (j + 1 < m_size) {
+					tmp[k++] = m_storage[++j];
+				}
 			}
 		}
-		delete[] m_storage;
-		m_storage = new T[--m_size];
-		for (size_t j = 0; j < m_size; ++j) {
-			m_storage[j] = tmp[j];
-		}
-		delete[] tmp;
-	} else {
+		if(m_storage)
+			delete[] m_storage;
+		m_storage = tmp;
+		--m_size;
+	}
+	else {
 		throw BadRemove();
 	}
 	return parametr;
@@ -89,9 +110,14 @@ template <typename T>
 void Arr<T>::addElement(const T elem) {
 	T* tmp = new T[m_size + 1];
 	tmp[m_size] = elem;
-	for (size_t i = 0; i < m_size; ++i) {
-		tmp[i] = m_storage[i];
+	/*for (size_t i = 0; i < m_size; ++i) {
+		tmp[i]= m_storage[i];
+	}*/
+	while (mark.canMove2next()) {
+		//*(tmp + (getMarker() - m_storage) ) = mark.getcur_val(); don't work
+		mark.move2next();
 	}
+	mark.rewind();
 	delete[] m_storage;
 	m_storage = tmp;
 	++m_size;
@@ -100,15 +126,16 @@ void Arr<T>::addElement(const T elem) {
 template <typename T>
 void Arr<T>::Resize_arr(const size_t new_size) {
 	T* tmp = new T[m_size];
-	for (size_t i = 0; i < m_size; ++i) {
+	/*for (size_t i = 0; i < m_size; ++i) {
 		tmp[i] = m_storage[i];
+	}*/
+	while (mark.canMove2next()) {
+		//*(tmp + (getMarker() - m_storage) ) = mark.getcur_val(); don't work
+		mark.move2next();
 	}
-	m_storage = new T[new_size];
-	m_size = new_size;
-	for (size_t i = 0; i < m_size; ++i) {
-		m_storage[i] = tmp[i];
-	}
-	delete[] tmp;
+	mark.rewind();
+	/*mark.m_storage = */m_storage = tmp;
+	/*mark.m_size = */m_size = new_size;
 }
 
 template <typename T>
@@ -116,4 +143,29 @@ void presentShelf(const Arr<T>& a, const size_t size) {
 	for (size_t i = 0; i < a.getSize; ++i) {
 		std::cout << a.getElement(i) << std::endl;
 	}
+}
+
+template <typename T>
+T& Marker<T>::getcur_val() {
+	return m_storage[m_cur_marker - m_storage];
+}
+
+template <typename T>
+void Marker<T>::move2next() {
+	++m_cur_marker;
+}
+
+template <typename T>
+bool Marker<T>::canMove2next() {
+	return (m_cur_marker < m_storage + m_size);
+}
+
+template <typename T>
+void Marker<T>::rewind() {
+	m_cur_marker = m_storage;
+}
+
+template <typename T>
+void Marker<T>::fast_forward() {
+	m_cur_marker = m_storage + m_size;
 }
---
+++ Class Arr/compiling.bat
@@ -0,0 +1,2 @@
+cl Ex_14_03.cpp /c /Fo Ex_14_03.obj Arr.cpp /c /Fo Arr.obj  stdafx.cpp /c /Fo stdafx.obj
+cl Ex_14_03.obj Arr.obj stdafx.obj /Fe: StopWar.exe
