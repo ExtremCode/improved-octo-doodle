90baac60b87c599e4cd6097fed9ff1fec8855214
Leff <klj2018@yandex.ru>
2022-03-27 17:05:45 +0300

    add tests and marker of array

--- Class Arr/Arr.h
+++ Class Arr/Arr.h
@@ -9,23 +9,29 @@ template <typename T>
 class Marker {
 	T* m_storage;
 	size_t m_size;
-protected:
 	T* m_cur_marker;
 public:
-	Marker(T* current = nullptr, T* store = nullptr, size_t size = 0) :
-		m_cur_marker(current), m_storage(store), m_size(size) {}
+	void setSize(size_t size) {
+		m_size = size;
+	}
+	void setData(T* data) {
+		m_storage = data;
+	}
 	T& getcur_val();
 	void rewind();
 	bool canMove2next();
 	void move2next();
 	void fast_forward();
+	T* getM_Mark() {
+		return m_cur_marker;
+	}
 };
 
 template<typename T>
-class Arr : public Marker<T> {
+class Arr {
 	T* m_storage;
-	size_t m_size;
-	Marker<T> mark{ m_storage, m_storage, m_size };
+	size_t m_size; 
+	Marker<T> mark;
 public:
 	Arr();
 	Arr(int);
@@ -36,14 +42,16 @@ public:
 	void addElement(const T);
 	void Resize_arr(const size_t);
 	T* getMarker() {
-		return mark.m_cur_marker;
+		return mark.getM_Mark();
 	}
 };
 
 template <typename T>
 Arr<T>::Arr() {
-	/*mark.m_size = */m_size = 0;
-	/*mark.m_cur_marker = mark.m_storage = */m_storage = nullptr;
+	m_size = 0;
+	mark.setSize(m_size);
+	m_storage = nullptr;
+	mark.setData(m_storage);
 }
 
 template <typename T>
@@ -52,8 +60,10 @@ Arr<T>::Arr(int size) {
 		throw BadSize();
 	}
 	else {
-		/*mark.m_size = */m_size = size;
-		/*mark.m_cur_marker = mark.m_storage = */m_storage = new T[m_size];
+		m_size = size;
+		mark.setSize(m_size);
+		m_storage = new T[m_size];
+		mark.setData(m_storage);
 	}
 }
 
@@ -72,11 +82,13 @@ T Arr<T>::removeElement(const size_t index) {
 		T* tmp = new T[m_size - 1];
 		for (size_t j = 0, k = 0; k < m_size; ++j) {
 			if (j != index) {
-				tmp[k++] = m_storage[j];
+				tmp[k] = m_storage[j];
+				++k;
 			}
 			else {
 				if (j + 1 < m_size) {
-					tmp[k++] = m_storage[++j];
+					tmp[k] = m_storage[++j];
+					++k;
 				}
 			}
 		}
@@ -108,39 +120,55 @@ size_t Arr<T>::getSize() const {
 
 template <typename T>
 void Arr<T>::addElement(const T elem) {
-	T* tmp = new T[m_size + 1];
-	tmp[m_size] = elem;
+	T* tmp = new T[++m_size];
+	tmp[m_size - 1] = elem;
 	/*for (size_t i = 0; i < m_size; ++i) {
 		tmp[i]= m_storage[i];
 	}*/
+	size_t k = 0;
+	mark.rewind();
 	while (mark.canMove2next()) {
-		//*(tmp + (getMarker() - m_storage) ) = mark.getcur_val(); don't work
+		tmp[k] = mark.getcur_val();
 		mark.move2next();
 	}
 	mark.rewind();
 	delete[] m_storage;
 	m_storage = tmp;
-	++m_size;
+	mark.setSize(m_size);
+	mark.setData(m_storage);
 }
 
 template <typename T>
 void Arr<T>::Resize_arr(const size_t new_size) {
-	T* tmp = new T[m_size];
-	/*for (size_t i = 0; i < m_size; ++i) {
-		tmp[i] = m_storage[i];
-	}*/
-	while (mark.canMove2next()) {
-		//*(tmp + (getMarker() - m_storage) ) = mark.getcur_val(); don't work
-		mark.move2next();
+	if (new_size > m_size) throw BadSize();
+	if (new_size == 0) {
+		m_storage = nullptr;
+		m_size = 0;
+		mark.setSize(m_size);
+		mark.setData(m_storage);
 	}
-	mark.rewind();
-	/*mark.m_storage = */m_storage = tmp;
-	/*mark.m_size = */m_size = new_size;
+	else {
+		T* tmp = new T[new_size];
+		/*for (size_t i = 0; i < m_size; ++i) {
+			tmp[i] = m_storage[i];
+		}*/
+		mark.rewind();
+		size_t k = 0;
+		while (mark.canMove2next() && k < new_size) {
+			tmp[k] = mark.getcur_val();
+			mark.move2next();
+		}
+		mark.rewind();
+		m_storage = tmp;
+		m_size = new_size;
+		mark.setSize(m_size);
+		mark.setData(m_storage);
+	}
 }
 
 template <typename T>
-void presentShelf(const Arr<T>& a, const size_t size) {
-	for (size_t i = 0; i < a.getSize; ++i) {
+void presentShelf(const Arr<T>& a) {
+	for (size_t i = 0; i < a.getSize(); ++i) {
 		std::cout << a.getElement(i) << std::endl;
 	}
 }
@@ -157,7 +185,7 @@ void Marker<T>::move2next() {
 
 template <typename T>
 bool Marker<T>::canMove2next() {
-	return (m_cur_marker < m_storage + m_size);
+	return (m_cur_marker - m_storage < m_size);
 }
 
 template <typename T>
---
+++ Class Arr/test.cpp
@@ -0,0 +1,96 @@
+#include "pch.h"
+#include "../../21_02/Arr.h"
+
+TEST(TestArr, adding) {
+	Arr<int> a{ 6 };
+	a.addElement(99);
+	EXPECT_EQ(a.getElement(6), 99);
+	EXPECT_EQ(a.getSize(), 7);
+
+	Arr<double> b{ 3 };
+	b.addElement(-7.3);
+	EXPECT_EQ(b.getElement(3), -7.3);
+	EXPECT_EQ(b.getSize(),4);
+	
+	Arr<char> c{ 7 };
+	c.addElement('z');
+	EXPECT_EQ(c.getElement(7), 'z');
+	EXPECT_EQ(c.getSize(),8);
+}
+
+TEST(TestArr, removing) {
+	Arr<float> a{ 6 };
+	a.addElement(-1.2f);
+	a.addElement(2.3f);
+	a.addElement(-3.6f);
+	a.addElement(4.2f);
+	a.addElement(12.5f);
+	a.removeElement(6);
+	// EXPECT_EQ(a.removeElement(6), -1.2f); left value is link so it's not equal to float number
+	EXPECT_EQ(a.getSize(), 10);
+
+	Arr<short> b{ 100 };
+	b.addElement((short)8);
+	b.addElement((short)3);
+	b.addElement(4);
+	b.addElement(9);
+	b.addElement(1);
+	b.removeElement(103);
+	EXPECT_EQ(b.getSize(), 104);
+
+	Arr<std::string> c{ 5 };
+	c.addElement("Just fine");
+	c.addElement("I lied");
+	c.addElement("I'm");
+	c.addElement("dying");
+	c.addElement("inside");
+	EXPECT_ANY_THROW(c.removeElement(11));
+}
+
+TEST(TestArr, getting) {
+	Arr<float> a{ 6 };
+	a.addElement(-3.6f);
+	a.addElement(12.5f);
+	EXPECT_ANY_THROW(a.getElement(8));
+
+	Arr<short> b{ 100 };
+	b.addElement((short)8);
+	b.addElement(3);
+	b.addElement(4);
+	b.addElement(9);
+	b.addElement(1);
+	EXPECT_EQ(b.getElement(103), 9);
+	EXPECT_EQ(b.getSize(), 105);
+
+	Arr<std::string> c{ 5 };
+	c.addElement("Just fine");
+	c.addElement("I lied");
+	c.addElement("I'm");
+	c.addElement("dying");
+	c.addElement("inside");
+	EXPECT_EQ(c.getElement(0),"I'm");
+	EXPECT_EQ(c.getElement(1), "I'm");
+	EXPECT_EQ(c.getElement(2), "I'm");
+	EXPECT_EQ(c.getElement(3), "I'm");
+	EXPECT_EQ(c.getElement(4), "I'm");
+	EXPECT_EQ(c.getElement(5), "I'm");
+	EXPECT_EQ(c.getElement(6), "I'm");
+	EXPECT_EQ(c.getElement(7), "I'm");
+	EXPECT_EQ(c.getElement(8), "I'm");
+	EXPECT_EQ(c.getElement(9), "I'm");
+	EXPECT_EQ(c.getSize(), 10);
+}
+
+TEST(TestArr, resizing) {
+	Arr<int> a{ 5 };
+	a.Resize_arr(4);
+	EXPECT_EQ(a.getSize(), 4);
+
+	Arr<double> b{ 3 };
+	b.Resize_arr(3);
+	EXPECT_EQ(b.getSize(), 3);
+
+	Arr<char> c{ 8 };
+	EXPECT_ANY_THROW(c.Resize_arr(10));
+	EXPECT_EQ(c.getSize(), 8);
+}
