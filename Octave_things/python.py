19462697f8e2458986523f93bd7f2423a7631045
ExtremCode <lev-moshkin@yandex.ru>
2023-05-28 17:52:35 +0300

    Update python.py

--- Octave_things/python.py
+++ Octave_things/python.py
@@ -5,24 +5,35 @@ from mpl_toolkits.mplot3d import axes3d
 import pandas as pd
 import seaborn as sb
 
-f = lambda y: np.array([10*(y[1]-y[0]), y[0]*(28-y[2])-y[1], y[0]*y[1]-8/3*y[2]])
-a = 0
-b = 10
-n = 50
-h = (b-a)/n
-t = np.zeros(n, dtype=float)
-t[0] = a
-y = np.zeros((3, n), dtype=float)
-y[:, 0] = [0, 1, 20]
-for j in range(n-1):
-    k1 = h*f(y[:, j])
-    k2 = h*f(y[:, j] + k1*h/2)
-    k3 = h*f(y[:, j] + k2*h/2)
-    k4 = h*f(y[:, j] + k3*h)
-    y[:, j+1] = y[:, j] + h/6*(k1 + 2*k2 + 2*k3 + k4)
-    t[j+1] = t[j]+h
-fig1, ax = plt.subplots()
-ax.plot(t,y[0,:])
-ax.plot(t,y[1,:])
-ax.plot(t,y[2,:])
+titanic = pd.read_csv('titanic.csv')
+print(titanic.shape)
+titanic['Relatives'] = titanic.SibSp + titanic.Parch
+#titanic.assign(relativ=(titanic.SibSp + titanic.Parch)>0)
+# male1 = titanic.query("Sex == 'male' and Survived == 1").Sex.count()
+# male0 = titanic.query("Sex == 'male' and Survived == 0").Sex.count()
+# women1 = titanic.query("Sex == 'female' and Survived == 1").Sex.count()
+# women0 = titanic.query("Sex == 'female' and Survived == 0").Sex.count()
+table = pd.pivot_table(titanic[['Survived','Sex']], index=['Sex'], columns=['Survived'], aggfunc=len)
+table.plot.pie(subplots=True, figsize=(4,4))
+#plt.show()
+
+titanic.loc[titanic.Pclass == 1, 'Pclass'] = 'буржуи'
+titanic.loc[titanic.Pclass == 2, 'Pclass'] = 'средний класс'
+titanic.loc[titanic.Pclass == 3, 'Pclass'] = 'пролетариат'
+lab = 'буржуи', 'средний класс', 'пролетариат'
+sizes=[titanic.loc[titanic.Pclass == 'буржуи', 'Pclass'].count(),
+        titanic.loc[titanic.Pclass == 'средний класс', 'Pclass'].count(),
+        titanic.loc[titanic.Pclass == 'пролетариат', 'Pclass'].count()]
+# Pclass bar chart
+fig = plt.figure(figsize=(6, 4))
+ax1 = fig.add_subplot()
+ax1.bar(lab,sizes)
+# before and after number of people bar chart
+fig = plt.figure()
+ax1 = fig.add_subplot()
+ax1.bar(['Before', 'After'], [titanic.shape[0], titanic.loc[titanic.Survived == 1].PassengerId.count()])
+# add new column about decision of price
+titanic['High_price'] = titanic.Fare.apply(lambda x: 'cheap' if x < 25 else 'expensive')
+# make histogram
+titanic.hist()
 plt.show()
